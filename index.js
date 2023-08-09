c31a6ef26c1a4bba0528c91ffc254b4b98df4ab3
eugene4545 <eugeneonuoha72@gmail.com>
2023-08-09 15:46:59 +0100

    algo-overview

---
+++ index.js
@@ -0,0 +1,60 @@
+/*
+let i =20;
+while (i < 10){
+    console.log(`While Loop Number : ${i}`)
+    i++;
+} 
+*/
+
+/*
+
+
+let e = 1
+if( e = 1){
+    return"elizabeth is beautiful";
+}else if (e > 3){
+    console.log("elizabeth is pretty");
+}else{
+    console.log("elizabeth is ugly");
+}*/
+
+
+/*
+const age = 18;
+if (age >= 18){
+    console.log("you can drive!")
+}else{
+    console.log(" werey! you cannot drive")
+}
+*/
+
+/*
+
+let color = "yellow";
+if (color === "red"){
+    console.log("color is red");
+}else if(color !== "blue" && color !== "red"){
+    console.log("not specified");
+}else if (color === "blue"){
+    console.log("its blue")
+}else{
+    console.log("color is blue")
+}
+*/
+
+
+//switch statements
+/*
+const man =27;
+if(man === 22)
+    return true
+}else{
+    return false
+}*/
+
+var a = 12
+var b =7
+var c = 20
+
+var sum = a+b+c
+console.log(sum)
